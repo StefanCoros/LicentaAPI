813b957f12ad46301166d86923aa5280affcbf19
Ion Bogatu <ionbogatu@gmail.com>
2023-08-27 12:14:54 +0300

    Change port from 3000 to 3001

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
   (swaggerUi = require("swagger-ui-express"));
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
   res.redirect("/api");
