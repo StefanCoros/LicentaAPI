efa416f2d2beacc8b81a2821c67d6cf5c1eea9dc
Ion Bogatu <ionbogatu@gmail.com>
2023-09-28 20:02:36 +0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://it-tracker-fe-13b5334e362a.herokuapp.com/'],
     credentials: true,
   });
 
